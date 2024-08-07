6e5a325aa4c9b11509b609aa1042605bcab09fa7
Matheus <galaxyplay41@gmail.com>
2024-08-07 16:41:22 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const cors = require("cors");
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/send', async (req, res) => {
+    const { name, email, text } = req.body;
+    console.log(name, email, text)
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+            user: process.env.EMAIL_USER, 
+            pass: process.env.EMAIL_PASS  
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER, 
+        to: 'galaxyplay41@gmail.com', 
+        replyTo: email,
+        subject: 'Teste de E-mail',
+        text: `Usuário com nome: ${name}, email: ${email} e mensagem: ${text}`,
+    };
+
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        res.send(info);
+    } catch (error) {
+        res.status(500).send(error.toString());
+    }
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
